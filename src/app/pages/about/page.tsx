8e91017bac4eb296fc9461e7e2bb08516845513d
Toan Vo <vothaitoan12@gmail.com>
2024-08-10 17:18:06 +0700

    style: add style for navbar

---
+++ src/app/pages/about/page.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/app/components/navbar";
+import Link from "next/link";
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <div>About us</div>
+      <Navbar />
+    </div>
+  );
+};
+
+export default About;
