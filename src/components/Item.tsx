25ada2cce23cf4b8e3f8da37a1c68b9ce0678631
Toan Vo <vothaitoan12@gmail.com>
2024-10-25 16:29:27 +0700

    feat: adjust changes in filter and item

--- src/components/Item.tsx
+++ src/components/Item.tsx
@@ -20,18 +20,20 @@ const Item = () => {
   return (
     <>
       <article
-        className="p-4 border-gray-300 border-[1px] flex flex-col gap-3 rounded-lg cursor-pointer hover:border-sky-600"
+        className="border-gray-300 border-[1px] flex flex-col gap-2 cursor-pointer hover:border-sky-600"
         onClick={showModal}
       >
         <Image
           src="https://www.mt.com/dam/homepage-redesign-2016-r01/thumbnails/esbu_industrial_ver2.jpg/_jcr_content/renditions/original.webp"
           alt="item"
-          className="w-full rounded-md"
+          className="w-full h-36"
           width={200}
           height={100}
         />
-        <h2 className="font-semibold font-customCardTitle text-xl">Title</h2>
-        <p>
+        <h2 className="font-semibold font-customCardTitle px-2 text-lg">
+          Title
+        </h2>
+        <p className="text-sm px-2 pb-2">
           Content: Lorem ipsum dolor sit amet consectetur adipisicing elit.
           Excepturi at quibusdam expedita provident quae atque facilis, eum
           incidunt ea, suscipit vero quaerat eaque sit possimus nostrum
@@ -45,6 +47,13 @@ const Item = () => {
         onOk={handleOk}
         onCancel={handleCancel}
       >
+        <Image
+          src="https://www.mt.com/dam/homepage-redesign-2016-r01/thumbnails/esbu_industrial_ver2.jpg/_jcr_content/renditions/original.webp"
+          alt="item"
+          className="w-full"
+          width={200}
+          height={100}
+        />
         <p>Some contents...</p>
         <p>Some contents...</p>
         <p>Some contents...</p>
