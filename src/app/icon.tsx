2d438de861daf4ce511cd078d6ff3a2bdbc7653f
Toan Vo <vothaitoan12@gmail.com>
2025-06-08 12:02:49 +0700

    fix: add icon tab , reduce size load image

---
+++ src/app/icon.tsx
@@ -0,0 +1,38 @@
+import { ImageResponse } from "next/og";
+import SwiperImg from "@/asset/swiper-img.webp";
+import Image from "next/image";
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          fontSize: 16,
+          background: "black",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          borderRadius: "50%",
+          color: "white",
+        }}
+      >
+        TT
+      </div>
+    ),
+    // ImageResponse options
+    {
+      // For convenience, we can re-use the exported icons size metadata
+      // config to also set the ImageResponse's width and height.
+      ...size,
+    }
+  );
+}
