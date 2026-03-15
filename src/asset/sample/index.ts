b310175e7b5e0bb821d6255fa9a7e722e34704b6
tona3922 <vothaitoan12@gmail.com>
2026-03-15 15:59:23 +0700

    Refactor code structure for improved readability and maintainability

--- src/asset/sample/index.ts
+++ src/asset/sample/index.ts
@@ -4,7 +4,11 @@ import can3 from "./can3.jpg";
 import can4 from "./can4.jpg";
 import can5 from "./can5.jpg";
 import can6 from "./can6.jpg";
-import can7 from "./can7.jpg";
+import can7 from "./can7.png";
 import can8 from "./can8.jpg";
+import can9 from "./can9.jpg";
+import can10 from "./can10.jpg";
+import can11 from "./can11.jpg";
+import can12 from "./can12.jpg";
 
-export const sampleImages = [can1, can2, can3, can4, can5, can6, can7, can8];
+export const sampleImages = [can1, can2, can3, can4, can5, can6, can7, can8, can9, can10, can11, can12];
